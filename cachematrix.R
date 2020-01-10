bbee9b13a17080c32b014557b8bdec52d69264f6
JongukPark <59676759+JongukPark@users.noreply.github.com>
2020-01-10 21:11:57 +0900

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,8 +4,45 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(y){
+                x <<- y
+                m <<- NULL
+        }
+        
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
 
 }
+
+
+cachemean <- function(x, ...) {
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- mean(data, ...)
+        x$setmean(m)
+        m
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 ## Write a short comment describing this function
